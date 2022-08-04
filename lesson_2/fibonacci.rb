dddfa26ce4c522ba16ad24b63c7e5a9cd5e538e9
11albina19 <11albina19@gmail.com>
2022-08-04 17:21:43 +0400

    lesson 2 (3/6)

---
+++ lesson_2/fibonacci.rb
@@ -0,0 +1,15 @@
+fibonacci = [0, 1]
+index     = 2
+x         = 0
+
+while x < 100 
+f1 = fibonacci [index-1].to_i
+f2 = fibonacci [index-2].to_i
+x  = f1 + f2 
+if x < 100
+fibonacci << x
+index     = index + 1
+end
+end
+
+print fibonacci
