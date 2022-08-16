6c5c4286600cf396875014ea914b71ea722d73f5
11albina19 <11albina19@gmail.com>
2022-08-16 12:15:16 +0400

    lesson 3 1.3

--- lesson_3/station.rb
+++ lesson_3/station.rb
@@ -1,7 +1,39 @@
 class Station
 
+  attr_reader :train_array
+
   def initialize (name)
-    @name = name
+    @name        = name
+    @train_array = []
   end
   
+  def to_accept_train (train)
+    puts('На станцию прибыл поезд ' + train.room.to_s)
+    @train_array << train
+  end
+
+  def show_trains 
+    puts self.train_array
+  end
+
+  def show_trains_type 
+
+    quantity_p = 0
+    quantity_c = 0
+    for index in @train_array
+      if index.passenger 
+        quantity_p += 1
+      else
+        quantity_c += 1
+      end
+    end 
+    puts('Количество пассажирских: ' + quantity_p.to_s + " , количество грузовых: " + quantity_c.to_s)
+  end
+
+  def send_train (train)
+    puts('Со станции отправляется поезд: ' + train.to_s)
+    #@train_array - [train]
+    @train_array.delete(train)
+  end
+
 end
--- lesson_3/train.rb
+++ lesson_3/train.rb
@@ -1,2 +1,13 @@
 class Train
+
+attr_reader :room
+
+attr_reader :passenger
+
+  def initialize (room, passenger, amount)
+    @room      = room
+    @passenger = passenger
+    @amount    = amount
+  end
+
 end
