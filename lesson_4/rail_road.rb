c51885443f7e7034a00b6a2b5005fddf1fdd6639
11albina19 <11albina19@gmail.com>
2023-01-19 21:14:52 +0400

    lesson 4 0.7

--- lesson_4/rail_road.rb
+++ lesson_4/rail_road.rb
@@ -25,250 +25,261 @@ class RailRoad
       puts "Введите 3, если вы хотите вывести текущие данные о объектах"
       puts "Введите 4, если хотите закончить программу"
       number = gets.chomp.to_i
-      if number == 4
-        puts "Выполнение программы завершено."
-        break
-      end
-      if number == 1
+      case number
+      when 1
         puts "-------------------------------------------------------------------"
         puts "Введите 0, если вы хотите создать станцию"
         puts "Введите 1, если вы хотите создать поезд"
         puts "Введите 2, если вы хотите создать вагон"
         puts "Введите 3, если вы хотите создать маршрут"
         puts "Введите 4 для возврата в предыдущее меню"
-        number_greate = gets.chomp.to_i
-        if number_greate == 0
-          greate_station
+        number_create = gets.chomp.to_i
+        case number_create
+        when 0
+          create_station
+        when 1
+          create_train
+        when 2
+          create_wagons
+        when 3
+          create_route
+        when 4
+          next
         else
-          if number_greate == 1
-            greate_train
-          else
-            if number_greate == 2
-              greate_wagons
-            else
-              if number_greate == 3
-                greate_route
-              else
-                if number_greate == 4
-                  next
-                else
-                  puts "Номер не определен. Выполнение программы завершено."
-                  break
-                end
-              end
-            end
-          end
+          puts "Номер не определен. Выполнение программы завершено."
+          break
         end
+      when 2
+        puts "-------------------------------------------------------------------"
+        puts "Введите 0, если вы хотите добавить/удалить станции в существующий маршрут"
+        puts "Введите 1, если вы хотите назначить маршрут поезду"
+        puts "Введите 2, если вы хотите изменить состав вагонов у поезда"
+        puts "Введите 3, если вы хотите переместить поезд по маршруту"
+        puts "Введите 4 для возврата в предыдущее меню"
+        number_change = gets.chomp.to_i
+        case number_change
+        when 0
+          add_stations_to_route
+        when 1
+          assign_route
+        when 2
+          change_wagons
+        when 3
+          move_train
+        when 4
+          next
+        else
+          puts "Номер не определен. Выполнение программы завершено."
+          break
+        end
+      when 3
+        puts "-------------------------------------------------------------------"
+        puts "Созданные станции и поезда на каждой из них:"
+        i = 0
+        for index in self.array_station
+          puts "Станция #{index.name}"
+          index.show_trains
+          i = i + 1
+        end
+      when 4
+        puts "Выполнение программы завершено."
+        break
       else
-        if number == 2
-          puts "-------------------------------------------------------------------"
-          puts "Введите 0, если вы хотите добавить/удалить станции в существующий маршрут"
-          puts "Введите 1, если вы хотите назначить маршрут поезду"
-          puts "Введите 2, если вы хотите изменить состав вагонов у поезда"
-          puts "Введите 3, если вы хотите переместить поезд по маршруту"
-          puts "Введите 4 для возврата в предыдущее меню"
-          number_change = gets.chomp.to_i
-          if number_change == 0
-            add_stations_to_route
-          else
-            if number_change == 1
-              assign_route
-            else
-              if number_change == 2
-                change_wagons
-              else
-                if number_change == 3
-                  move_train
-                else
-                  if number_greate == 4
-                    next
-                  else
-                    puts "Номер не определен."
-                  end
-                end
-              end
-            end
-          end
-        else
-          if number == 3
-            puts "-------------------------------------------------------------------"
-            puts "Созданные станции и поезда на каждой из них:"
-            i = 0
-            for index in self.array_station
-              puts "Станция #{index.name}"
-              index.show_trains
-              i = i + 1
-            end
-          else
-            puts "Номер не определен."
-          end
-        end
+        puts "Номер не определен. Выполнение программы завершено."
+        break
       end
     end
   end
 
-  def greate_station
+  def create_station
     puts "Как назвать станцию?"
     name = gets.chomp
-    st = Station.new(name)
-    self.array_station << st
-    puts "Создана станция " + st.name + " !"
+    station = Station.new(name)
+    self.array_station << station
+    puts "Создана станция " + station.name + " !"
   end
 
-  def greate_train
+  def create_train
     puts "Это пассажирский поезд? 1 - пассажирский, 2 - грузовой"
     number_passenger = gets.chomp.to_i
     puts "Введите номер поезда"
     train_room = gets.chomp.to_i
     if number_passenger == 1
-      t = PassengerTrain.new(train_room)
-      self.array_train << t
-      puts "Создан поезд " + t.room.to_s
+      train = PassengerTrain.new(train_room)
+      self.array_train << train
+      puts "Создан поезд " + train.room.to_s
+    elsif number_passenger == 2
+      train = CargoTrain.new(train_room)
+      self.array_train << train
+      puts "Создан поезд " + train.room.to_s
     else
-      if number_passenger == 2
-        t = CargoTrain.new(train_room)
-        self.array_train << t
-        puts "Создан поезд " + t.room.to_s
-      else
-        puts "Номер не определен."
-      end
+      puts "Номер не определен"
     end
   end
 
-  def greate_wagons
+  def create_wagons
     puts "Это пассажирский вагон? 1 - пассажирский, 2 - грузовой"
     number_passenger = gets.chomp.to_i
     puts "Введите номер вагона"
     wagons_room = gets.chomp.to_i
     if number_passenger == 1
-      w = PassengerWagons.new(wagons_room)
-      self.array_wagons << w
-      puts "Создан вагон " + w.room.to_s
+      wagons = PassengerWagons.new(wagons_room)
+      self.array_wagons << wagons
+      puts "Создан вагон " + wagons.room.to_s
+    elsif number_passenger == 2
+      wagons = CargoWagons.new(wagons_room)
+      self.array_wagons << wagons
+      puts "Создан вагон " + wagons.room.to_s
     else
-      if number_passenger == 2
-        w = CargoWagons.new(wagons_room)
-        self.array_wagons << w
-        puts "Создан вагон " + w.room.to_s
-      else
-        puts "Номер не определен."
-      end
+      puts "Номер не определен"
     end
   end
+end
 
-  def greate_route
-    puts "Перед вами список всех доступных станций:"
-    show_all_station
-    puts "Введите индекс начальной станции маршрута"
-    index_begin = gets.chomp.to_i
-    puts "Введите индекс конечной станции маршрута"
-    index_end = gets.chomp.to_i
-    r = Route.new(array_station[index_begin], array_station[index_end])
-    self.array_route << r
-    puts "Создан маршрут! Его станции: "
-    r.show_all
+def create_route
+  puts "Перед вами список всех доступных станций:"
+  show_all_station
+  puts "Введите индекс начальной станции маршрута"
+  index_begin = gets.chomp.to_i
+  begin_selection_error = type_check(array_station, index_begin)
+  if begin_selection_error
+    return
   end
+  puts "Введите индекс конечной станции маршрута"
+  index_end = gets.chomp.to_i
+  end_selection_error = type_check(array_station, index_end)
+  if end_selection_error
+    return
+  end
+  route = Route.new(array_station[index_begin], array_station[index_end])
+  self.array_route << route
+  puts "Создан маршрут! Его станции: "
+  route.show_all
+end
 
-  def add_stations_to_route
-    puts "Какое изменение требуется? 1 - добавить станцию, 2 - удалить станцию"
-    number = gets.chomp.to_i
-    puts "Перед вами список всех доступных маршрутов:"
-    show_all_route
-    puts "Введите индекс выбранного маршрута:"
-    index_route = gets.chomp.to_i
-    puts "Перед вами список всех доступных станций:"
-    show_all_station
-    puts "Введите индекс выбранной станции"
-    index_station = gets.chomp.to_i
-    if number == 1
-      array_route[index_route].add(array_station[index_station])
-    else
-      if number == 2
-        array_route[index_route].delete(array_station[index_station])
-      end
-    end
-    puts "Станции маршрута после изменения:"
-    array_route[index_route].show_all
+def add_stations_to_route
+  puts "Какое изменение требуется? 1 - добавить станцию, 2 - удалить станцию"
+  number = gets.chomp.to_i
+  puts "Перед вами список всех доступных маршрутов:"
+  show_all_route
+  puts "Введите индекс выбранного маршрута:"
+  index_route = gets.chomp.to_i
+  route_selection_error = type_check(array_route, index_route)
+  if route_selection_error
+    return
   end
+  puts "Перед вами список всех доступных станций:"
+  show_all_station
+  puts "Введите индекс выбранной станции"
+  index_station = gets.chomp.to_i
+  station_selection_error = type_check(array_station, index_station)
+  if station_selection_error
+    return
+  end
+  if number == 1
+    array_route[index_route].add(array_station[index_station])
+  elsif number == 2
+    array_route[index_route].delete(array_station[index_station])
+  end
+  puts "Станции маршрута после изменения:"
+  array_route[index_route].show_all
+end
 
-  def assign_route
-    puts "Перед вами список всех доступных поездов:"
-    show_all_train
-    puts "Введите индекс выбранного поезда:"
-    index_train = gets.chomp.to_i
-    puts "Перед вами список всех доступных маршрутов:"
-    show_all_route
-    puts "Введите индекс выбранного маршрута:"
-    index_route = gets.chomp.to_i
-    array_train[index_train].attach_route(array_route[index_route])
-    puts "Поезду #{array_train[index_train].room}, пассажирский: #{array_train[index_train].to_s},"
-    puts "назначен маршрут #{array_route[index_route]}"
+def assign_route
+  puts "Перед вами список всех доступных поездов:"
+  show_all_train
+  puts "Введите индекс выбранного поезда:"
+  index_train = gets.chomp.to_i
+  train_selection_error = type_check(array_train, index_train)
+  if train_selection_error
+    return
   end
+  puts "Перед вами список всех доступных маршрутов:"
+  show_all_route
+  puts "Введите индекс выбранного маршрута:"
+  index_route = gets.chomp.to_i
+  route_selection_error = type_check(array_route, index_route)
+  if route_selection_error
+    return
+  end
+  array_train[index_train].attach_route(array_route[index_route])
+  puts "Поезду #{array_train[index_train].room}, пассажирский: #{array_train[index_train].to_s},"
+  puts "назначен маршрут #{array_route[index_route]}"
+end
 
-  def change_wagons
-    puts "Какое изменение требуется? 1 - добавить вагон, 2 - удалить вагон"
-    number = gets.chomp.to_i
-    puts "Перед вами список всех доступных поездов:"
-    show_all_train
-    puts "Введите индекс выбранного поезда:"
-    index_train = gets.chomp.to_i
-    puts "Перед вами список всех доступных вагонов:"
-    show_all_wagons
-    puts "Введите индекс выбранного вагона"
-    index_wagons = gets.chomp.to_i
-    if number == 1
-      array_train[index_train].attach_wagons(array_wagons[index_wagons])
-    else
-      if number == 2
-        array_train[index_train].unhook_wagons(array_wagons[index_wagons])
-      end
-    end
+def change_wagons
+  puts "Какое изменение требуется? 1 - добавить вагон, 2 - удалить вагон"
+  number = gets.chomp.to_i
+  puts "Перед вами список всех доступных поездов:"
+  show_all_train
+  puts "Введите индекс выбранного поезда:"
+  index_train = gets.chomp.to_i
+  train_selection_error = type_check(array_train, index_train)
+  if train_selection_error
+    return
   end
+  puts "Перед вами список всех доступных вагонов:"
+  show_all_wagons
+  puts "Введите индекс выбранного вагона"
+  index_wagons = gets.chomp.to_i
+  wagons_selection_error = type_check(array_wagons, index_wagons)
+  if wagons_selection_error
+    return
+  end
+  if number == 1
+    array_train[index_train].attach_wagons(array_wagons[index_wagons])
+  elsif number == 2
+    array_train[index_train].unhook_wagons(array_wagons[index_wagons])
+  end
+end
 
-  def move_train
-    puts "Какое перемещение поезда требуется? 1 - вперед, 2 - назад"
-    number = gets.chomp.to_i
-    puts "Перед вами список всех доступных поездов:"
-    show_all_train
-    puts "Введите индекс выбранного поезда:"
-    index_train = gets.chomp.to_i
-    if number == 1
-      array_train[index_train].forward
-    else
-      if number == 2
-        array_train[index_train].back
-      end
-    end
+def move_train
+  puts "Какое перемещение поезда требуется? 1 - вперед, 2 - назад"
+  number = gets.chomp.to_i
+  puts "Перед вами список всех доступных поездов:"
+  show_all_train
+  puts "Введите индекс выбранного поезда:"
+  index_train = gets.chomp.to_i
+  train_selection_error = type_check(array_train, index_train)
+  if train_selection_error
+    return
   end
+  if number == 1
+    array_train[index_train].forward
+  elsif number == 2
+    array_train[index_train].back
+  end
+end
 
-  def show_all_station
-    i = 0
-    for index in self.array_station
-      puts "Станция #{index.name}, #{index}, ИНДЕКС #{i}"
-      i = i + 1
-    end
+def show_all_station
+  array_station.each_with_index do |station, index|
+    puts "Станция #{station.name}, ИНДЕКС #{index}"
   end
+end
 
-  def show_all_train
-    i = 0
-    for index in self.array_train
-      puts "Поезд #{index.room}, пассажирский: #{index.passenger.to_s}, #{index}, ИНДЕКС #{i}"
-      i = i + 1
-    end
+def show_all_train
+  array_train.each_with_index do |train, index|
+    puts "Поезд #{train.room}, пассажирский: #{train.passenger.to_s}, ИНДЕКС #{index}"
   end
+end
 
-  def show_all_wagons
-    i = 0
-    for index in self.array_wagons
-      puts "Вагон #{index.room}, пассажирский: #{index.passenger.to_s}, #{index}, ИНДЕКС #{i}"
-      i = i + 1
-    end
+def show_all_wagons
+  array_wagons.each_with_index do |wagons, index|
+    puts "Вагон #{wagons.room}, пассажирский: #{wagons.passenger.to_s}, ИНДЕКС #{index}"
   end
+end
 
-  def show_all_route
-    i = 0
-    for index in self.array_route
-      puts "Маршрут #{index}, ИНДЕКС #{i}"
-      i = i + 1
-    end
+def show_all_route
+  array_route.each_with_index do |route, index|
+    puts "Маршрут, ИНДЕКС #{index}"
   end
+end
+
+def type_check(array, index)
+  element = array[index]
+  error = element.nil?
+  if error
+    puts "Ошибка в выборе элемента! Выполняется возврат в предыдущее меню..."
+  end
+  error
 end
